a34cae204fbc4f15aa01afa1d12abee36c23d400
relecteve <Filonovaval@gmail.com>
2021-01-22 21:08:42 +0300

    ага

--- src/view/add-new-event.js
+++ src/view/add-new-event.js
@@ -3,9 +3,9 @@ import {DEFAULT_OFFERS} from "../const.js";
 
 const defaultEvent = () => {
   return {
-    // pointRoute: generateRandomElement(POINTS_ROUTE),
-    // nameRoute: generateRandomElement(NAMES_ROUTE),
-    // isFavorite: Boolean(getRandomInteger(0, 1)),
+    pointRoute: `Flight`,
+    nameRoute: `Geneva`,
+    destination: null,
     dateFrom: dayjs().toDate(),
     dateTo: dayjs().toDate(),
     offers: DEFAULT_OFFERS,
@@ -14,25 +14,44 @@ const defaultEvent = () => {
 
 console.log(defaultEvent());
 
-export const createAddNewEventTemplate = () => {
-  const {pointRoute, nameRoute, price, isFavorite, dateFrom, dateTo, offers} = defaultEvent;
+export const createAddNewEventTemplate = (event) => {
+  const {pointRoute, nameRoute, dateFrom, dateTo, offers, destination} = defaultEvent;
 
   const createDestinationTemplate = () => {
+    if (destination === null) {
+      return;
+    }
+
     return `
     <section class="event__section  event__section--destination">
       <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-      <p class="event__destination-description">${destination}</p>
+      <p class="event__destination-description"></p>
 
       <div class="event__photos-container">
         <div class="event__photos-tape">
-          <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
         </div>
       </div>
     </section>`;
+  };
+
+  const createOfferTemplate = (title, price) => {
+    return (`<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
+    <label class="event__offer-label" for="event-offer-luggage-1">
+      <span class="event__offer-title">${title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${price}</span>
+    </label>
+  </div>`);
+  }
+
+  const createOffersTemplate = () => {
+    let result = offers.reduce(function(layot, offer) {
+      const {title, price} = offer;
+      return layot + createOfferTemplate(title, price);
+    }, ``);
+
+    return result;
   };
 
   return `<li class="trip-events__item">
@@ -104,9 +123,9 @@ export const createAddNewEventTemplate = () => {
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+          ${pointRoute}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${nameRoute}" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -138,50 +157,7 @@ export const createAddNewEventTemplate = () => {
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
         <div class="event__available-offers">
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-            <label class="event__offer-label" for="event-offer-luggage-1">
-              <span class="event__offer-title">Add luggage</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">30</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-            <label class="event__offer-label" for="event-offer-comfort-1">
-              <span class="event__offer-title">Switch to comfort class</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">100</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-            <label class="event__offer-label" for="event-offer-meal-1">
-              <span class="event__offer-title">Add meal</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">15</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-            <label class="event__offer-label" for="event-offer-seats-1">
-              <span class="event__offer-title">Choose seats</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">5</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-            <label class="event__offer-label" for="event-offer-train-1">
-              <span class="event__offer-title">Travel by train</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">40</span>
-            </label>
-          </div>
+        ${createOffersTemplate()}
         </div>
       </section>
       ${createDestinationTemplate()}
